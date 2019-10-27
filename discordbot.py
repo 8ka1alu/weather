6a561b405c4bdb8f244553cd0efc202a4e5230ce
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-10-28 08:30:38 +0900

    discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -75,9 +75,6 @@ async def on_message(message):
     if client.user in message.mentions: # 話しかけられたかの判定
         reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
         await message.channel.send(reply) # 返信メッセージを送信
-
-    if message.content == '/members':
-        print(member_name)
     
 @client.event
 async def on_member_join(member):
