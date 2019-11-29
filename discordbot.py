3d4a891ebaaa030eb0909fdf396863f1adc91b01
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-11-29 12:55:24 +0900

    discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -46,7 +46,7 @@ async def on_message(message):
         await channel.send('<@&613342519438344193> 2時間たちました！') 
 
     if message.content.startswith("23"): 
-        await channel.send('2時間たちました！') 
+        await message.channel.send('2時間たちました！') 
 
     if message.author.bot:  # ボットのメッセージをハネる
         return
