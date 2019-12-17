0de8a154b485ffad21b3d60757080fe90da7652c
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-12-17 21:08:58 +0900

    discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -38,9 +38,9 @@ async def on_ready():
     print('Hello World,リマインドbotプログラム「project-RRN」、起動しました')
     channel = client.get_channel(CHANNEL_ID2)
     await channel.purge()
-    await channel.send(client.user.name)  # ボットの名前
-    await channel.send(client.user.id)  # ボットのID
-    await channel.send(discord.__version__)  # discord.pyのバージョン
+    await channel.send(f'名前:{client.user.name}')  # ボットの名前
+    await channel.send(f'ID:{client.user.id}')  # ボットのID
+    await channel.send(f'Discord ver:{discord.__version__}')  # discord.pyのバージョン
     await channel.send('------')
     await channel.send('BOT再起動しました。')   
     await client.change_presence(activity=discord.Game(name='ギルド専属ナビ'))
