ad1e78fc8b0384ab71369a9a1719898ccc6fd4ea
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-12-22 17:05:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -76,7 +76,7 @@ async def on_message(message):
             member_count_server = len(message.guild.members) -5
             await message.channel.send(f'今のサーバー人数：{member_count_server}人')
             await message.channel.send(f'総チャンネル数：{len(message.guild.channels)}個')
-            await message.channel.send(f'総チャンネル数：{len(message.guild.bot)}個')
+            await message.channel.send(f'総bot数：{len(message.guild.bots)}個')
             await message.channel.send(f'テキストチャンネル数：{len(message.guild.text_channels)}個')
             await message.channel.send(f'ボイスチャンネル数：{len(message.guild.voice_channels)}個')
             embed = discord.Embed(title="サーバーアイコン")
