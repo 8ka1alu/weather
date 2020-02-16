8eaaa5dff26b8847f7ee5857607023a26f0e80e4
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-02-16 16:06:13 +0900

    Update weather.py

--- weather.py
+++ weather.py
@@ -54,8 +54,9 @@ async def on_message(message):
         msg += "の天気は、\n"
         for f in resp['forecasts']:
           msg += f['dateLabel'] + "が" + f['telop'] + "\n"
-        msg += "です。"
+        msg += "です。\n```"
         msg += resp['description']['text']
+        msg += "```"
 
         await message.channel.send(message.author.mention + msg)
 
